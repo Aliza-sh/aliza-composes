41e38e0cf3585025eb484a6c0a1c75155531893a
Aliza <alizashahsavari@gmail.com>
2024-08-13 23:30:08 +0330

    Wallet Compose: Step: add Resource,Apps Note: 1-Add project photos.Tourism Compose:

--- app/src/main/java/com/aliza/alizacomposes/main/Apps.kt
+++ app/src/main/java/com/aliza/alizacomposes/main/Apps.kt
@@ -9,5 +9,5 @@ import com.aliza.alizacomposes.wallet.WalletActivity
 sealed class Apps(val imageLight: Int,val imageDark: Int, val activity: ComponentActivity) {
     data object Instagram : Apps(R.drawable.instagram_light,R.drawable.instagram_dark, InstagramActivity())
     data object Tourism : Apps(R.drawable.tourist_light,R.drawable.tourist_dark, TourismActivity())
-    data object Wallet : Apps(R.drawable.img_samplebanner,R.drawable.img_samplebanner, WalletActivity())
+    data object Wallet : Apps(R.drawable.wallet_light,R.drawable.wallet_dark, WalletActivity())
 }
