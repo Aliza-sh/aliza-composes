3a2d4ab216a4839644e8913e8ce4c746f07b8fbd
Aliza <alizashahsavari@gmail.com>
2024-08-18 02:43:21 +0330

    Shoes Compose: HeaderWidget: Step: add resource,HeaderWidget,ShoesScreen Note: 1-Design HeaderWidget of ShoesScreen.

--- app/src/main/java/com/aliza/alizacomposes/shoes/ui/ShoesScreen.kt
+++ app/src/main/java/com/aliza/alizacomposes/shoes/ui/ShoesScreen.kt
@@ -7,12 +7,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.aliza.alizacomposes.shoes.ui.components.HeaderWidget
 
 @Composable
 fun ShoesScreen() {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
-
+            HeaderWidget()
         }
     }
 }
