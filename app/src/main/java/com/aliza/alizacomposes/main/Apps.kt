41152a603c2f42443f30f2e82220867c303ad0a7
Aliza <alizashahsavari@gmail.com>
2024-08-18 03:16:30 +0330

    Shoes Compose: Step: add Resource,Apps Note: 1-Add project photos.

--- app/src/main/java/com/aliza/alizacomposes/main/Apps.kt
+++ app/src/main/java/com/aliza/alizacomposes/main/Apps.kt
@@ -19,5 +19,5 @@ sealed class Apps(val imageLight: Int,val imageDark: Int, val activity: Componen
     data object MyOs : Apps(R.drawable.myos_light,R.drawable.myos_dark, MyOsActivity())
     data object Movies : Apps(R.drawable.movies,R.drawable.movies, MoviesActivity())
     data object Vpn : Apps(R.drawable.vpn,R.drawable.vpn, VpnActivity())
-    data object Shoes : Apps(R.drawable.img_samplebanner,R.drawable.img_samplebanner, ShoesActivity())
+    data object Shoes : Apps(R.drawable.shoes,R.drawable.shoes, ShoesActivity())
 }
